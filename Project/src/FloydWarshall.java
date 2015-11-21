23b9aaf2a84c63252213a54c58ae4530c05f44da
Elie Antoun <egantoun@wireless-10-146-48-232.public.utexas.edu>
2015-11-20 20:21:03 -0600

    Lol

---
+++ Project/src/FloydWarshall.java
@@ -0,0 +1,79 @@
+import com.sun.deploy.panel.AbstractRadioPropertyGroup;
+
+import java.util.ArrayList;
+
+public class FloydWarshall {
+
+    public static void getPairwiseShortestPath(boolean[][] adjacencyMatrix, int [][] minimumPairwiseDistances, int [][] nextHopEvent)
+    {
+
+        int numberOfEvents = adjacencyMatrix.length; //Number of evenst in the computation
+        //Initialize return 2-D array to Integer.MAX_VALUE
+        //Initialize next hop event to -1
+
+        for(int i=0; i<numberOfEvents; i++)
+        {
+            for (int j = 0; j < numberOfEvents; j++)
+            {
+                minimumPairwiseDistances[i][j] = Integer.MAX_VALUE;
+                nextHopEvent[i][j] = -1;
+            }
+        }
+
+        //Set distance to self to be equal to 0
+        for(int i=0; i<numberOfEvents; i++)
+        {
+            minimumPairwiseDistances[i][i] = 0;
+        }
+
+        //For each edge (u,v) set distance to 1
+        for(int i=0; i<numberOfEvents; i++)
+        {
+            for(int j=0; j<numberOfEvents; j++)
+            {
+                if(adjacencyMatrix[i][j])
+                {
+                    minimumPairwiseDistances[i][j] =1;
+                    nextHopEvent[i][j] = j;
+                }
+            }
+        }
+
+        //Get minimum path between pairwise events
+        for(int k=0; k<numberOfEvents; k++)
+        {
+            for(int i=0; i<numberOfEvents; i++)
+            {
+                for(int j=0; j<numberOfEvents; j++)
+                {
+                    if(minimumPairwiseDistances[i][j] > minimumPairwiseDistances[i][k] + minimumPairwiseDistances[k][j]) {
+                        minimumPairwiseDistances[i][j] = minimumPairwiseDistances[i][k] + minimumPairwiseDistances[k][j];
+                        nextHopEvent[i][j] = nextHopEvent[i][k];
+                    }
+                }
+            }
+        }
+
+    }
+
+
+    public  static ArrayList<Integer> getPath(int[][] nextHopEvents, int from, int to)
+    {
+        ArrayList<Integer> resultPath = new ArrayList<>();
+        if(nextHopEvents[to][from] == -1)
+        {
+            return resultPath;
+        }
+
+        resultPath.add(from);
+        while(from != to)
+        {
+            from = nextHopEvents[from][to];
+            resultPath.add(to);
+        }
+
+        return resultPath;
+
+    }
+
+}
---
+++ Project/src/PredicateControl.java
@@ -0,0 +1,36 @@
+/**
+ * Created by egantoun on 11/20/15.
+ */
+
+import java.util.*;
+public class PredicateControl {
+
+    public ArrayList<Integer> getShortestSequence (ArrayList<Integer> initialEvents, ArrayList<Integer> finalEvents, boolean [][] adjacencyMatrix)
+    {
+        int numberOfEvents = adjacencyMatrix.length;
+        int[][] pairwiseShortestPath = new int[numberOfEvents][numberOfEvents];
+        int[][] nextHopEvents =  new int[numberOfEvents][numberOfEvents];
+        FloydWarshall.getPairwiseShortestPath(adjacencyMatrix,pairwiseShortestPath,nextHopEvents);
+
+        //Start with initializing shortest path from inital to final
+        ArrayList<Integer> resultShortestPath = null;
+        int minDistanceSoFar = Integer.MAX_VALUE;
+
+        for(Integer initialEvent: initialEvents)
+        {
+            for(Integer finalEvent: finalEvents)
+            {
+                if(pairwiseShortestPath[initialEvent][finalEvent] < minDistanceSoFar)
+                {
+                    minDistanceSoFar = pairwiseShortestPath[initialEvent][finalEvent];
+                    resultShortestPath = FloydWarshall.getPath(nextHopEvents,initialEvent, finalEvent);
+                }
+            }
+        }
+
+        return resultShortestPath;
+
+
+    }
+}
+
